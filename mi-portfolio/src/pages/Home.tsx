09368bc217cf2b0e47384aa458ade4a240b11b75
Osel Nicolás <oselko123@gmail.com>
2026-09-18 12:35:11 +0200

    feat: configurar react-router con layout y 4 rutas principales.

---
+++ mi-portfolio/src/pages/Home.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1 className="text-white text-3x1 p-8">Inicio</h1>
+}
---
+++ mi-portfolio/src/pages/Projects.tsx
@@ -0,0 +1,3 @@
+export default function Projects() {
+    return <h1 className="text-white text-3x1 p-8">Proyectos</h1>
+}

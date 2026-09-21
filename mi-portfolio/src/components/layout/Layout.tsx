3425be60dc93fa2cdf5512d48050956f8669652d
Osel Nicolás <oselko123@gmail.com>
2026-09-21 12:53:51 +0200

    feat: integrar Navbar y Footer en Layout con flex-1 para footer anclado

--- mi-portfolio/src/components/layout/Layout.tsx
+++ mi-portfolio/src/components/layout/Layout.tsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout() {
     return(
-        <div className="min-h-screen bg-slate-900 text-white">
-            {/*Aquí irá <Navbar /> más adelante */}
-            <Outlet />
-            {/*Aquí irá <Footer /> más adelante */}
+        <div className="flex flex-col min-h-screen bg-slate-900 text-white">
+            <Navbar />
+            <main className="flex-1">
+                <Outlet />
+            </main>
+            <Footer />
         </div>
     )
 }
